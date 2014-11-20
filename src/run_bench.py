928cc770326d3dcc0d11a40b8ae5ed8c833acead
behr <maximilian.behr@st.ovgu.de>
2014-11-20 12:33:14 +0100

    reorganizing

--- src/run_bench.py
+++ src/run_bench.py
@@ -1,34 +1,45 @@
 import os
 
-if ".." not in os.environ['PYTHONPATH']:
-    os.environ['PYTHONPATH'] = "..:" + os.environ['PYTHONPATH']
+# Default options
+OPTIONS = {"refinement_level": 0,
+           "dt_division": 0,
+           "save_solution": True,
+           "save_frequency": 20,
+           "check_mem_usage": True,
+           "check_frequency": 10,
+           "save_solution_at_t=T": True,
+           "save_xml": True,
+           "compute_stress": True,
+           "compute_divergence": True,
+           "debug": False,
+           "max_steps": None,
+           "krylov_solver_absolute_tolerance": 1e-25,
+           "krylov_solver_relative_tolerance": 1e-12,
+           "krylov_solver_monitor_convergence": False,
+}
 
 
+parameters["refinement_algorithm"] = "bisection"
+if __name__=="__main__":
 
-#test problems from nsbench
-instant_clean = False
-refinements = [3]
-problems = ["beltrami", "drivencavity"]
-solvers = ["chorin", "css1", "css2", "ipcs"]
-for refinement in refinements:
-    for problem in problems:
-        for solver in solvers:
-            if instant_clean:
-                os.system("instant-clean")
-            print "python ns.py {0} {1} refinement_level={2} debug=True".format(problem, solver, str(refinement))
-            os.system("python ns.py {0} {1} refinement_level={2} debug=True".format(problem, solver, str(refinement)))
+    #test problems from nsbench
+    instant_clean = False
+    refinements = [3]
+    problems = ["beltrami", "drivencavity"]
+    solvers = ["chorin", "css1", "css2", "ipcs"]
 
-#karman
-instant_clean = False
-REs = [100, 200, 300, 400, 500]
-refinements = [2, 3, 4, 5]
-problems = ["karman"]
-solvers = ["chorin", "css1", "css2", "ipcs", "stat_newton"]
-for RE in REs:
     for refinement in refinements:
         for problem in problems:
             for solver in solvers:
                 if instant_clean:
                     os.system("instant-clean")
-                print "python ns.py {0} {1} refinement_level={2} RE={3} debug=True".format(problem, solver, str(refinement),RE)
-                os.system("python ns.py {0} {1} refinement_level={2} RE={3} debug=True".format(problem, solver, str(refinement),RE))
+                print "python ns.py {0} {1} refinement_level={2} debug=True".format(problem, solver, str(refinement))
+                os.system("python ns.py {0} {1} refinement_level={2} debug=True".format(problem, solver, str(refinement)))
+
+
+
+
+
+
+
+
