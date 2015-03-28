41eb3d8a96acf6093eef3176d09e1b724b5eb1d7
maxb <maximilian.behr@st.ovgu.de>
2015-03-28 23:59:05 +0100

    small improvment on ctrl simulation

--- fenics_linux.sh
+++ fenics_linux.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 source venv/bin/activate
 export PYTHONPATH=`pwd`:${PYTHONPATH}
-export LD_LIBRARY_PATH=/home/daniels/workspace/mess_debug/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/daniels/build/usr/local/lib/python2.7/site-packages:${PYTHONPATH}
+export LD_LIBRARY_PATH=/home/daniels/build/mess_build_debug/lib:${LD_LIBRARY_PATH}
 
+echo "Numpy in use:"
+python -c "import numpy; print numpy.__file__; numpy.__config__.show()"
+
+echo "Scipy in use:"
+python -c "import scipy; print scipy.__file__; scipy.__config__.show()"
+
+echo "Pycmess in use:"
+python -c "import pycmess; print pycmess.__file__"
--- fenics_mpi.sh
+++ fenics_mpi.sh
@@ -3,3 +3,13 @@
 
 
 export PYTHONPATH=`pwd`:${PYTHONPATH}
+
+
+echo "Numpy in use:"
+python -c "import numpy; print numpy.__file__; numpy.__config__.show()"
+
+echo "Scipy in use:"
+python -c "import scipy; print scipy.__file__; scipy.__config__.show()"
+
+echo "Pycmess in use:"
+python -c "import pycmess; print pycmess.__file__"
